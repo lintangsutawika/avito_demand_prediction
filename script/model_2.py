ca514d6dbac29a0e05ee550ce7c46c7ed2874ccd
Lintang Adyuta Sutawika <lintang@konvergen.ai>
2018-06-18 12:18:12 +0700

    with binary logloss predictions

--- script/model_2.py
+++ script/model_2.py
@@ -1059,7 +1059,7 @@ lgbm_params =  {
     # 'metric': 'binary_logloss',
     'metric': metric,
     # 'max_depth': 15,
-    'num_leaves':400,
+    'num_leaves':450,
     'feature_fraction': 0.5,
     'bagging_fraction': 0.75,
     # 'min_data_in_leaf': 500,
