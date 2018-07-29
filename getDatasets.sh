cc20cb7e096844f5ff67f445cead7eb6152db28a
Lintang Sutawika <lintangsutawika@gmail.com>
2018-07-30 02:23:49 +0700

    modified to put downloaded files in respective directories

--- getDatasets.sh
+++ getDatasets.sh
@@ -1,24 +1,23 @@
-mkdir input/image-confidence
-kaggle kernels output -k sukhyun9673/export-image-features -p .
-mv *_blurrness.csv input/image-confidence
+mkdir input
+mkdir input/avito-demand-prediction
+kaggle competitions download -c avito-demand-prediction -f train.csv -p input/avito-demand-prediction
+kaggle competitions download -c avito-demand-prediction -f test.csv -p input/avito-demand-prediction
 
-kaggle kernels output -k sukhyun9673/extracting-image-features-test -p .
-mv Image_KP_SCORES.csv input/image-confidence
+mkdir input/image-confidence
+kaggle kernels output -k sukhyun9673/export-image-features -p input/image-confidence
+
+kaggle kernels output -k sukhyun9673/extracting-image-features-test -p input/image-confidence
 
 mkdir input/aggregated
-kaggle kernels output -k bminixhofer/aggregated-features-lightgbm -p .
-rm submission.csv
-mv aggregated-features.csv input/aggregated
+kaggle kernels output -k bminixhofer/aggregated-features-lightgbm -p input/aggregated
+rm input/aggregated/submission.csv
 
 mkdir input/text2image-top-1
-kaggle kernels output -k christofhenkel/text2image-top-1 -p .
-rm embedding_dict.p model.hdf5
-mv *_image_top_1_features.csv input/text2image-top-1
+kaggle kernels output -k christofhenkel/text2image-top-1 -p input/text2image-top-1
+rm input/text2image-top-1/embedding_dict.p input/text2image-top-1/model.hdf5
 
 mkdir input/image-features/vgg-test
-kaggle kernels output -k bguberfain/vgg16-test-features -p .
-mv features.npz input/image-features/vgg-test
+kaggle kernels output -k bguberfain/vgg16-test-features -p input/image-features/vgg-test
 
 mkdir input/image-features/vgg-train
-kaggle kernels output -k bguberfain/vgg16-train-features -p .
-mv features.npz input/image-features/vgg-train
+kaggle kernels output -k bguberfain/vgg16-train-features -p input/image-features/vgg-train
